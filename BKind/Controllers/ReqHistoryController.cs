4c8e0b2fc1cdbfae88ba1bf71ea0a4ee09824a32
Felicia Cordos <co.el0154629s@stud.portalid.ubbcluj.ro>
2020-05-08 20:49:19 +0300

    add requests to ReqHistory

--- BKind/Controllers/ReqHistoryController.cs
+++ BKind/Controllers/ReqHistoryController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using BKind.Data;
 using BKind.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BKind.Controllers
 {
@@ -38,32 +41,34 @@ namespace BKind.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AddReqToHistory(int RequestId, int CategoryId, int PersonId, string Title, string Description, string ImagePath )
+        public async Task<IActionResult> AddReqToHistory([Bind("ID","CategoryID","Description",
+            "ImagePath","PersonID","Title")]Request model)        
         {
-            var currentUser = await _userManager.GetUserAsync(User);
-            var userId = await _userManager.GetUserIdAsync(currentUser);
+                var currentUser = await _userManager.GetUserAsync(User);
+                var userId = await _userManager.GetUserIdAsync(currentUser);
 
-            var newReqHistory = new ReqHistory()
-            {
-                UserId = userId,
-                RequestId = RequestId,
-                CategoryID = CategoryId,
-                PersonID = PersonId,
-                Title = Title,
-                Description = Description,
-                ImagePath = ImagePath
-            };
-            _context.Add(newReqHistory);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index", "Requests");
+                var newReqHistory = new ReqHistory()
+                {
+                    UserId = userId,
+                    RequestId = model.ID,
+                    CategoryID = model.CategoryID,
+                    Description = model.Description,
+                    ImagePath = model.ImagePath,
+                    PersonID = model.PersonID,
+                    Title = model.Title,
 
+                };
+
+                _context.Add(newReqHistory);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Requests");
         }
-
 
         // POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EliminateReqFromHistory(int requestId)
+        public async Task<IActionResult> RemoveReqFromHistory(int requestId)
         {
             var reqHistory = await _context.ReqHistory.FindAsync(_userManager.GetUserId(User), requestId); //FindAsync primeste cheia compusa formata din cele 2 id-uri
             _context.ReqHistory.Remove(reqHistory);
